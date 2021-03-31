956958bcff577539f834da047925c27bc732b10e
zhqu1148980644 <zhongquan789@gmail.com>
2021-03-31 14:15:17 +0800

    update tests/test_remote_import

---
+++ flowsaber/utility/__init__.py
@@ -0,0 +1,1 @@
+import httpimport as load
--- tests/test_remote_import.py
+++ tests/test_remote_import.py
@@ -1,11 +1,10 @@
-import flowsaber
 from flowsaber import *
 from flowsaber.utility import load
 
 
 def test_remote_import():
-    url = "https://gist.githubusercontent.com/zhqu1148980644" \
-          "/2eafbe8d25883919ecf11a729f1fdb9a/raw/28c1be8056cb0ac46da227c0e537107b99952f0f"
+    url = "https://gist.githubusercontent.com/zhqu1148980644/" \
+          "2eafbe8d25883919ecf11a729f1fdb9a/raw/742c8ada5f70589dd6ec83641462f88aa36a6b27"
     with load.remote_repo(['testtask'], url):
         from testtask import task1, task2
 
